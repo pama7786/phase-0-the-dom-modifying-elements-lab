550b246747dc737ad437b5f7ea47aab29897b42f
Abdirahman abass diis <abdirahmanfarmajo7786@gmail.com>
2023-06-23 23:22:23 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// index.js
+
+// Remove the <main> element with id 'main'
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new header element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new header to 'victory'
+newHeader.id = 'victory';
+
+// Set the inner text of the new header to 'YOUR-NAME is the champion'
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new header to the document body
+document.body.appendChild(newHeader);
